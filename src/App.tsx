22ef0fc1384cdd260d3d5bd5c58faee06e472014
Wenderson <wps713@gmail.com>
2021-06-14 09:19:20 -0300

    Changing app content  to content component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 import { useEffect, useState } from 'react';
 
-import { Button } from './components/Button';
-import { MovieCard } from './components/MovieCard';
-
 import { SideBar } from './components/SideBar';
 import { Content } from './components/Content';
 
@@ -65,19 +62,7 @@ export function App() {
         handleClickButton={handleClickButton} 
         selectedGenreId={selectedGenreId}
       />
-      <div className="container">
-        <header>
-          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
-        </header>
-
-        <main>
-          <div className="movies-list">
-            {movies.map(movie => (
-              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
-            ))}
-          </div>
-        </main>
-      </div>
+     <Content movies={movies} selectedGenre={selectedGenre}/>
     </div>
   )
 }
